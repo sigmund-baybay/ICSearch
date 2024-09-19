ee96b1b703b59181c2dcd787b611a9ebf2d1c5dc
sigmund-baybay <sobaybay@up.edu.ph>
2024-09-20 01:23:20 +0800

    feat: adjusted home page, started search-bar javascript component

---
+++ js/app.js
@@ -0,0 +1,23 @@
+function getResult(){
+    const searchType = document.getElementById('search-type').value;
+    const searchResult = document.getElementById('search-bar').value;
+    
+    if (searchType == "Professor"){
+        findProfessor(searchResult);
+
+    }else if (searchType == "Room"){
+        getRoom(searchResult);
+
+    }
+
+}
+
+function findProfessor(searchResult) {
+    searchResult;
+
+}
+
+function getRoom(searchResult) {
+    searchResult;
+
+}
