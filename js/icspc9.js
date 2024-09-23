a0300521688aae30db58eb9720b3120d67bf190b
scpepito <scpepito@up.edu.ph>
2024-09-24 00:18:31 +0800

    feat: added ICS PC Lab 9

---
+++ js/icspc9.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const images = ['../../src/floorplan/1 Stairs.png', '../../src/floorplan/2 Stairs.png', '../../src/floorplan/3 PC 9.png'];
+    const titles = ['1st Floor', '2nd Floor', '3rd Floor'];
+    let currentIndex = 0;
+
+    function updateImageAndButtons() {
+        document.getElementById('floorImage').src = images[currentIndex];
+       
+        document.querySelector('.floor-count h3').textContent = titles[currentIndex];
+
+        document.getElementById('prevButton').disabled = (currentIndex === 0);
+        document.getElementById('nextButton').disabled = (currentIndex === images.length - 1);
+    }
+
+    document.getElementById('prevButton').addEventListener('click', () => {
+        if (currentIndex > 0) {
+            currentIndex--;
+            updateImageAndButtons();
+        }
+    });
+
+    document.getElementById('nextButton').addEventListener('click', () => {
+        if (currentIndex < images.length - 1) {
+            currentIndex++;
+            updateImageAndButtons();
+        }
+    });
+
+    updateImageAndButtons();
+});
