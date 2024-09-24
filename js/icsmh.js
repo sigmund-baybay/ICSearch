29590f0253c2fbd5d7ce8bc9ff7d0e808cc6e209
Rafael Zerna <nvzerna@up.edu.ph>
2024-09-24 21:17:10 +0800

    js: improved floor switching buttons

--- js/icsmh.js
+++ js/icsmh.js
@@ -5,26 +5,22 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function updateImageAndButtons() {
         document.getElementById('floorImage').src = images[currentIndex];
-       
         document.querySelector('.floor-count h3').textContent = titles[currentIndex];
-
-        document.getElementById('prevButton').disabled = (currentIndex === 0);
-        document.getElementById('nextButton').disabled = (currentIndex === images.length - 1);
     }
 
     document.getElementById('prevButton').addEventListener('click', () => {
-        if (currentIndex > 0) {
+        if (currentIndex > 0) 
             currentIndex--;
-            updateImageAndButtons();
-        }
+        else 
+            currentIndex = images.length-1;
+        updateImageAndButtons();
     });
 
     document.getElementById('nextButton').addEventListener('click', () => {
-        if (currentIndex < images.length - 1) {
+        if (currentIndex < images.length - 1) 
             currentIndex++;
-            updateImageAndButtons();
-        }
+        else
+            currentIndex = 0;
+        updateImageAndButtons();
     });
-
-    updateImageAndButtons();
 });
